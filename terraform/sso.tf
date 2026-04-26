3b0b0ce29c8cebfd6da2eb6d03af92e1bcb6a699
Graham Brooks <graham.brooks@avatarfleet.com>
2026-04-25 19:54:28 -0700

    refactor: use local sso_instance_arn for managed policy and account assignments

--- terraform/sso.tf
+++ terraform/sso.tf
@@ -135,13 +135,13 @@ resource "aws_ssoadmin_permission_set" "administrator" {
 }
 
 resource "aws_ssoadmin_managed_policy_attachment" "administrator" {
-  instance_arn       = aws_ssoadmin_instance.main.arn
+  instance_arn       = local.sso_instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.administrator.arn
   managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_ssoadmin_account_assignment" "graham_admin" {
-  instance_arn       = aws_ssoadmin_instance.main.arn
+  instance_arn       = local.sso_instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.administrator.arn
   principal_id       = aws_identitystore_user.graham.user_id
   principal_type     = "USER"
